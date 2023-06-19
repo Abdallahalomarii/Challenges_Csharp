e400ebdd59c059966dc128d2ac12c21aaed97519
abood alomary <abboodoa@gmail.com>
2023-06-19 03:03:55 +0300

    Add project files.

---
+++ TestMethods/UnitTest1.cs
@@ -0,0 +1,7 @@
+namespace TestMethods
+{
+    public class UnitTest1
+    {
+        // write code here
+    }
+}
---
+++ lab-03-Challanges/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab_03_Challanges
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
